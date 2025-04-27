494d48d192841bffc3abd14e7a588ce5b9ebf2c1
tecdevetec <lucashfdantas@gmail.com>
2025-04-27 11:47:48 -0300

    Formulário Cadastro Produto/Funcionário

--- cadastrarFuncionario.php
+++ cadastrarFuncionario.php
@@ -1,1 +1,43 @@
-<h1>cadastrar Funcionário</h1>
+<div class="form-container-funcionario">
+    
+    <form action="salvar_produto.php" method="POST">
+    
+        <label >Cargo</label>
+        <input type="text"  name="descricao" required>
+
+        <label">Data de Admissão:</label>
+        <input type="date"  name="data_chegada">
+
+        <label >Data de Recisão:</label>
+        <input type="date"  name="data_saida">
+
+        <label >Data de Nascimento:</label>
+        <input type="date"  name="data_chegada">
+        
+        <label>Código Pessoa:</label>
+        <input type="text"  name="categoria">
+
+        <label>Nome:</label>
+        <input type="text"  name="categoria">
+
+        <label>Endereço:</label>
+        <input type="text"  name="categoria">
+
+        <label>CEP:</label>
+        <input type="text"  name="categoria">
+
+        <label>Telone:</label>
+        <input type="text"  name="categoria">
+
+        <label>CPF:</label>
+        <input type="text"  name="categoria">
+
+        <label>Tipo:</label>
+        <input type="text"  name="categoria">
+
+
+
+
+        <button type="submit">Cadastrar Funcionàrio</button>
+    </form>
+</div>
--- cadastrarProduto.php
+++ cadastrarProduto.php
@@ -1,1 +1,21 @@
-<h1>cadastrar Produto</h1>
+<div class="form-container-produto">
+    <form action="salvar_produto.php" method="POST">
+    
+        <label >Descrição:</label>
+        <input type="text"  name="descricao" required>
+
+        <label >Data de Chegada:</label>
+        <input type="date"  name="data_chegada">
+
+        <label >Data de Saída:</label>
+        <input type="date"  name="data_saida">
+
+        <label >Data da Vistoria:</label>
+        <input type="date"  name="data_vistoria">
+
+        <label>Categoria:</label>
+        <input type="text"  name="categoria">
+
+        <button type="submit">Cadastrar Produto</button>
+    </form>
+</div>
